34f411a8ef3720d2c80ec40304155ae217dc33fe
Kuldeep Singh <ks2sept2005ks@gmail.com>
2024-07-18 22:01:13 +0530

    more change

--- src/component/snippet-edit-form.tsx
+++ src/component/snippet-edit-form.tsx
@@ -1,23 +1,22 @@
-'use client';
+"use client";
 
-import type { Snippet } from '@prisma/client';
-import Editor from '@monaco-editor/react';
-import { useState } from 'react';
-import *  as actions from '@/actions'
+import type { Snippet } from "@prisma/client";
+import Editor from "@monaco-editor/react";
+import { useState } from "react";
+import * as actions from "@/actions";
 interface SnippetEditFormProps {
   snippet: Snippet;
 }
 
 export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
-    const [code, setCode] = useState(snippet.code);
-    const handleEditorChange = (value:string = "") => {
-         setCode(value)
-    } 
-    const editSnippetAction = actions.editSnippet.bind(null, snippet.id, code);
+  const [code, setCode] = useState(snippet.code);
+  const handleEditorChange = (value: string = "") => {
+    setCode(value);
+  };
+  const editSnippetAction = actions.editSnippet.bind(null, snippet.id, code);
   return (
-    <div className='my-4 bg-black'>Edit your snippet
-      <div className='m-10 '>
-
+    <div className="my-4 bg-black">
+      Edit your snippet
       <Editor
         height="40vh"
         theme="vs-dark"
@@ -25,11 +24,13 @@ export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
         defaultValue={snippet.code}
         options={{ minimap: { enabled: false } }}
         onChange={handleEditorChange}
-        />
-          <form action={editSnippetAction}>
-              <button type='submit' className='p-2 border rounded text-xl'> Save</button>
-          </form>
-        </div>
-      </div>
+      />
+      <form action={editSnippetAction}>
+        <button type="submit" className="p-2 border rounded text-xl">
+          {" "}
+          Save
+        </button>
+      </form>
+    </div>
   );
 }
